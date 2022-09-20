921904209512e7468e2a9d88efee77868129d3a0
J1UN <109601025+J1UN@users.noreply.github.com>
2022-09-20 23:50:28 +0900

    Update bioinfomatics_3_6_seq.py

--- bioinfomatics_3_6_seq.py
+++ bioinfomatics_3_6_seq.py
@@ -1,12 +1,12 @@
 import re
 
 fasta= open("sequence.protein.fasta",'r')
-
 data= fasta.readlines()
+
+title= data[0]
 prod_1= data[1:]
 
 prod_2= ''.join(prod_1)
-prod_3 = prod_2.replace("\n","")
-seq= str(prod_3)
+seq = prod_2.replace("\n","")
 
 fasta.close()
