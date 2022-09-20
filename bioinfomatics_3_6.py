67f14220a3ae73df463686fd4e2be1cb100779e7
J1UN <109601025+J1UN@users.noreply.github.com>
2022-09-20 23:49:37 +0900

    Update bioinfomatics_3_6.py

--- bioinfomatics_3_6.py
+++ bioinfomatics_3_6.py
@@ -7,10 +7,9 @@ title= data[0]                              #title이라는 변수에 첫줄(0) 
 
 prod_1= data[1:]                            #fasta seq의 첫줄(0)인 타이틀 제외
 prod_2= ''.join(prod_1)                     #title을 제외한 line들을 list에서 string으로 join
-prod_3 = prod_2.replace("\n","")            #string으로 join한 seq의 개행문자 제거
-seq= str(prod_3)                            #개행제거한 seq을 str로 반환 
-
-print("title: ",data[0])                    #타이틀출력
+seq= prod_2.replace("\n","")                #string으로 join한 seq의 개행문자 제거
+ 
+print("title: ",title)                      #타이틀출력
 print("seq: ",seq)                          #연속된 seq 출력
 
 fasta.close()                               #open한 파일 닫기
