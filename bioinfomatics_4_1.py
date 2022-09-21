a0970c66659d04ee17513c51c605238e2f6ee8ac
J1UN <109601025+J1UN@users.noreply.github.com>
2022-09-21 17:48:30 +0900

    Update bioinfomatics_4_1.py

--- bioinfomatics_4_1.py
+++ bioinfomatics_4_1.py
@@ -1,11 +1,14 @@
+import re
+
 gb = open("sequence.protein.gb",'r')
 
 data= gb.readlines()
 title = data[0]
-str_data=str(data)
+str_data=''.join(data)
+str_data2= str_data.relace('\n','')
 
 seq_start= int(str_data.index("ORIGIN"))
-seq= str_data[(seq_start)+6:]
+seq= str_data2[(seq_start)+6:]
 
 
 print("title: ",title)
