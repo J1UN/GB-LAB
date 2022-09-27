92f3bf62c4638f1ef68413bec57dc369a5e9c49d
J1UN <109601025+J1UN@users.noreply.github.com>
2022-09-27 11:24:01 +0900

    Add files via upload

---
+++ DNA_basecode.py
@@ -0,0 +1,11 @@
+base={'F':['TTT','TTC'],'L':['TTA','TTG','CTT','CTC','CTA','CTG'],'I':['ATT','ATC','ATA'],'M':['ATG'],'V':['GTT','GTC','GTA','GTG'],'S':['TCT','TCC','TCA','TCG','AGT','AGC']
+      ,'P':['CCT','CCC','CCA','CCG'], 'T':['ACT','ACC','ACA','ACG'], 'A':['GCT','GCC','GCA','GCG'],'Y':['TAT','TAC'],'*':['TAA','TAG','TGA'],'H':['CAT','CAC']
+      ,'Q':['CAA','CAG'],'N':['AAC','AAT'],'K':['AAA','AAG'],'D':['GAT','GAC','GAA'], 'E':['GAG'], 'C':['TGT','TGC'],'W':['WGG'],'R':['CGT','CGC','CGA','CGG','AGA','AGG']
+      ,'G':['GGC','GGA','GGG']}
+
+Base={'TTT':'F','TTC':'F','TTA':'L','TTG':'L', 'CTT':'L','CTC':'L','CTA':'L','CTG':'L','ATT':'I','ATC':'I','ATA':'I','ATG':'M','GTT':'V'
+      , 'GTC':'V','GTA':'V','GTG':'V','TCT':'S','TCC':'S','TCA':'S','TCG':'S','CCT':'P','CCC':'P','CCA':'P','CCA':'P','CCG':'P','ACT':'T'
+      , 'ACC':'T','ACA':'T','ACG':'T','GCT':'A','GCC':'A','GCA':'A','GCG':'A','TAT':'Y','TAC':'Y','TAA':'*','TAG':'*','TGA':'*',
+      'CAT':'H','CAC':'H','CAA':'Q','CAG':'Q','AAT':'N','AAC':'N','AAA':'K','AAG':'K','GAT':'D','GAC':'D','GAA':'D','GAG':'E','TGT':'C'
+      , 'TGC':'C','TGG':'W','CGT':'R','CGC':'R','CGA':'R','CGG':'R','AGT':'S','AGC':'S','AGA':'R','AGG':'R','GGT':'G','GGC':'G','GGA':'G'
+      , 'GGG':'G'}
