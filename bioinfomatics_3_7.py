a0149568f46f5997907fdf8840820e732a96e336
J1UN <109601025+J1UN@users.noreply.github.com>
2022-10-06 14:32:36 +0900

    Update bioinfomatics_3_7.py

--- bioinfomatics_3_7.py
+++ bioinfomatics_3_7.py
@@ -1,8 +1,8 @@
 from bioinfomatics_3_6_seq import seq 
 
-aa_num = int(input("Position: "))               # 아미노산의 위치 입력
+aa_num = int(input("Position: ")) -1               # 아미노산의 위치 입력
 
 aa_seq= seq[aa_num:aa_num+3]                    # 입력 위치로부터 3번째까지의 문자열(아미노산)을 aa_seq에 저장
 
 
-print("Three amino acids: ",aa_seq)             
+print("Three amino acids: ", aa_seq)             
